f1f9fde1ec2682b9d36c8e4298ceeecf833b828c
choocher13 <keane.67@buckeyemail.osu.edu>
2020-02-21 17:50:14 -0500

    some groundwork is all

--- PW Please/src/pwplease/pwplease.java
+++ PW Please/src/pwplease/pwplease.java
@@ -1,10 +1,30 @@
 package pwplease;
 
+import java.util.Scanner;
+
 public class pwplease {
 
 	public static void main(String[] args) {
-		System.out.println("REached this point!!!");
+		System.out.println("Welcome to Password, Please");
+		Scanner in = new Scanner(System.in);
+		String command = in.nextLine();
+		while (!command.equals("QUIT")) {
+			if (command.equals("HELP")) {
+				printCommands();
+			} else if (command.equals("SOMETHING HERE")) {
 
+			} else if (command.equals("SOMETHING ELSE HERE")) {
+				// ect ect
+			} else {
+				System.out.println("INVALID COMMAND");
+			}
+		}
+		in.close();
+	}
+
+	private static void printCommands() {
+		System.out.println(
+				"HELP, APPROVE, DENY, LIST, PASSWORD, EXCRYPTED PASSWORD, DECRYPT, SEND TOKEN, SCAN FACE, SECURITY QUESTION, TIME");
 	}
 
 }
