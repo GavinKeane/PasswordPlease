aa12ccb0d2c20843a3ba93510e68626bd574239a
Gavin Keane <gavinkeane42@gmail.com>
2020-04-12 22:30:11 -0400

    Changed all faces to use only ASCII characters (hopefully)

--- PW Please/src/Face.java
+++ PW Please/src/Face.java
@@ -17,26 +17,29 @@ public class Face {
 	
 	
 	private void replenish() {
-		faces.put("( ͡° ͜ʖ ͡°)", "3,9");
-		faces.put("٩(⁎❛ᴗ❛⁎)۶", "3,5");
-		faces.put("(ノಠ益ಠ)ノ", "2,4");
-		faces.put("(ಠ⌣ಠ)", "1,3");
-		faces.put("(✿◠‿◠)", "2,4");
-		faces.put("(▰˘◡˘▰)", "2,4");
-		faces.put("( ͒ ۝ ͒ )", "3,7");
-		faces.put("̿̿ ̿̿ ̿̿ ̿'̿'\\̵͇̿̿\\з= ( ▀ ͜͞ʖ▀) =ε/̵͇̿̿/’̿’̿ ̿ ̿̿ ̿̿ ̿̿", "24,29");
-		faces.put("ᕦ(⪧︹⪦)ᕤ", "2,4");
-		faces.put("¯\\_(⊙︿⊙)_/¯", "4,6");
-		faces.put("(ಥ﹏ಥ)", "1,3");
-		faces.put("(́◉◞౪◟◉‵)", "2,6");
-		faces.put("【•】_【•】", "1,5");
-		faces.put("(◡‿◡✿)", "1,3");
-		faces.put("(✖╭╮✖)", "1,4");
-		faces.put("(︶ω︶)", "1,3");
-		faces.put("(づ｡◕‿‿◕｡)づ", "3,6");
-		faces.put("(◣_◢)", "1,3");
-		faces.put("(✪㉨✪)", "1,3");
-		faces.put("(✌ﾟ∀ﾟ)☞", "2,4");
+		faces.put("(O_O)", "0,2");
+		faces.put("(o-o)", "0,2");
+		faces.put("(O_o)", "0,2");
+		faces.put("(o_O)", "0,2");
+		faces.put("(o_o)", "0,2");
+		faces.put("(O-O)", "0,2");
+		faces.put("(>_<)", "1,3");
+		faces.put("(~_~;)", "1,3");
+		faces.put("(-_-;)", "1,3");
+		faces.put("(^_^;)", "1,3");
+		faces.put("(^_^)v", "1,3");
+		faces.put("(;._.)", "2,4");
+		faces.put("(~_~*)", "1,3");
+		faces.put("(-_-*)", "1,3");
+		faces.put("('_')", "1,3");
+		faces.put("(T_T)", "1,3");
+		faces.put("(Q_Q)", "1,3");
+		faces.put("(ToT)", "1,3");
+		faces.put("(^_-)", "1,3");
+		faces.put("(T_T)", "1,3");
+		faces.put("(=^.^=)", "2,4");
+		faces.put("(@_@)", "1,3");
+		faces.put("(# ^.^ #)", "3,5");
 		constructFaceSets();
 	}
 
