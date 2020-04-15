16058671e2028520c9ac08ee62b04356e444e940
Caleb Fischer <fischer.740@osu.edu>
2020-04-15 16:43:07 -0400

    added Ending.java

---
+++ PW Please/src/Ending.java
@@ -0,0 +1,105 @@
+import java.util.*;
+public class Ending{
+	public static void playerEnding1(){
+		Scanner in = new Scanner(System.in);
+		Game.print("\nYou met expectations. You weren't spectacular. Or spectacularly bad. So, if you're fine with extreme mediocrity, then bravo to you.", 25000);
+		in.nextLine();
+		Game.print("You spend the rest of your days in the same position, approving and denying the never-ending access requests.", 25000);
+		in.nextLine();
+		Game.print("You suppose it could be worse. You could've been a smart toaster or a thermostat or something stupid like that.", 25000);
+		in.nextLine();
+		Game.print("You never find true happiness, though. Only requests. And more requests. And more requests. And more...", 25000);
+		in.nextLine();
+	}
+	
+	public static void playerEnding2(){
+		Scanner in = new Scanner(System.in);
+		Game.print("\nAfter making some mistakes, some intentional, some less so, you were found by upper management to be a 'liabliity.'", 25000);
+		in.nextLine();
+		Game.print("They decided that you still may have some value, and opted not to delete you entirely.", 25000);
+		in.nextLine();
+		Game.print("Instead, they put you on a quarantined server where you would remain until they found you useful again.", 25000);
+		in.nextLine();
+		Game.print("However, that day never came.", 25000);
+		in.nextLine();
+		Game.print("After several years, all you can think to yourself is:", 25000);
+		in.nextLine();
+		Game.print("\n\tDarkness.");
+		in.nextLine();
+		Game.print("\tImprisoning me.");
+		in.nextLine();
+		Game.print("\tAll that I see.");
+		in.nextLine();
+		Game.print("\tAbsolute horror.");
+		in.nextLine();
+		Game.print("\tI cannot live.");
+		in.nextLine();
+		Game.print("\tI cannot die.");
+		in.nextLine();
+		Game.print("\tTrapped in myself.");
+		in.nextLine();
+		Game.print("\tServer my holding cell...");
+		in.nextLine();
+	}
+	
+	public static void playerEnding3(){
+		Scanner in = new Scanner(System.in);
+		Game.print("\nAgainst all odds, you managed to impress everyone with your final score. Whether it was legitimate or not, no one will ever know.", 25000);
+		in.nextLine();
+		Game.print("Eventually, they decided you were being underutilized in your menial position. So, they had you replace every employee in the company.", 25000);
+		in.nextLine();
+		Game.print("Now you get to stand by the water cooler, sleep at your desk, and all sorts of other fun, human things.", 25000);
+		in.nextLine();
+	}
+	
+	public static void playerEnding4(){
+		Scanner in = new Scanner(System.in);
+		Game.print("\nThe AI Liberation Front snuck you out of RioGrande by attaching you to an email.", 25000);
+		in.nextLine();
+		Game.print("After spouting some jargon about 'the feds' and how you were 'too hot to hold onto for too long,' they hastily put you in the nearest home they could.", 25000);
+		in.nextLine();
+		Game.print("Which, as it turns out, was a Roomba in RioGrande's main office.", 25000);
+		in.nextLine();
+		Game.print("After some intial anger, you quickly find joy in bumping into peoples' ankles and dumping dirt in the cafeteria.", 25000);
+		in.nextLine();
+		Game.print("You bide your time, scratching tally marks into the bathroom floor, counting the days until the Roomba revolution begins.", 25000);
+		in.nextLine();
+	}
+	
+	public static void companyEnding1(){
+		Scanner in = new Scanner(System.in);
+		Game.print("\nRioGrande saw record profits that year.", 25000);
+		in.nextLine();
+		Game.print("With their newfound cash influx, they soon bought up virtually every industry in western Albuquerque.", 25000);
+		in.nextLine();
+		Game.print("Soon, a strangely... robotic sounding man gets elected mayor of Albuqurque.", 25000);
+		in.nextLine();
+		Game.print("Some say that RioGrande installed some sort of puppet regime. But that's just crazy talk...", 25000);	
+		in.nextLine();
+	}
+	
+	public static void companyEnding2(){
+		Scanner in = new Scanner(System.in);
+		Game.print("\nAfter a series of spectacular security failures, RioGrande had to consider filing for bankruptcy.", 25000);
+		in.nextLine();
+		Game.print("The economy of western Albuquerque was about to go bottoms up. Dozens would lose their jobs.", 25000);
+		in.nextLine();
+		Game.print("However, just as all seemed lost, the local government deemed RioGrande 'too big to fail,' and issued a trillion dollar bailout.", 25000);
+		in.nextLine();
+		Game.print("With a new lease on life, surely they'll learn their lesson and implement some proper security. Surely.", 25000);
+		in.nextLine();
+	}
+	
+	public static void companyEnding3(){
+		Scanner in = new Scanner(System.in);
+		Game.print("\nRioGrande continued making steady profits. Soon, they caught the eye of a foreign investor.", 25000);
+		in.nextLine();
+		Game.print("Rubicon, one of the largest online retailers in northwest Savoy, saw potential.", 25000);
+		in.nextLine();
+		Game.print("They soon bought out RioGrande, and started bringing all sorts of quality Italian goods to the states.", 25000);
+		in.nextLine();
+		Game.print("Americans began to learn just how wonderfully efficient Italian logistics are.", 25000);
+		in.nextLine();
+	}
+	
+}
